fdab498dec1015ff4ee59f67bf6d4976966f9367
Ranc58 <rancvova@gmail.com>
2017-09-19 16:36:01 +0300

    total rebuild.

--- verb_counter/verb_counter.py
+++ verb_counter/verb_counter.py
@@ -1,7 +1,7 @@
-import sys
 import ast
 import os
 import collections
+import argparse
 from nltk import pos_tag
 
 PROJECTS = [
@@ -60,6 +60,14 @@ def get_trees(files_content_list):
     return trees
 
 
+def get_all_names(trees):
+    all_names = []
+    for tree in trees:
+        all_names.append(node.id for node in ast.walk(tree)
+                         if isinstance(node, ast.Name))
+    return all_names
+
+
 def get_func_names(trees):
     func_names_list = []
     for tree in trees:
@@ -81,14 +89,43 @@ def verb_check(word):
     return word_type == 'VB'
 
 
-def get_verbs(verbs):
-    return collections.Counter(verbs)
+def print_all_project_funcs_names(funcs_names_list):
+    print('All funcs names:')
+    for func in funcs_names_list:
+        print('"{func}"'.format(func=func))
 
 
-if __name__ == '__main__':
-    addon_projects = sys.argv[1:]
-    all_projects = get_all_projects(addon_projects)
+def print_total_funcs_names_info(all_funcs_names):
+    print('total funcs names: {count}\nunique funcs names: {unique}'.format(
+        count=len(all_funcs_names), unique=len(set(all_funcs_names))))
+
+
+def print_verbs_info(top_verbs):
+    print('total verbs: {words}\nunique verbs: {unique}'.format(
+        words=len(top_verbs), unique=len(set(top_verbs))))
+    for word, occurence in collections.Counter(
+            top_verbs).most_common():
+        print('"{verb}" in {occurence} projects'.format(verb=word,
+                                                        occurence=occurence))
+
+
+def create_parser_for_user_arguments():
+    parser = argparse.ArgumentParser(description='Work with funcs names.')
+    parser.add_argument('-a', '--all_names', action='store_true',
+                        help='Print ')
+    parser.add_argument('-p', '--projects', nargs='+', required=False,
+                        type=str, help='Adding projects')
+    return parser.parse_args()
+
+
+def output_to_cli():
+    user_argument = create_parser_for_user_arguments()
+    if user_argument.projects:
+        all_projects = get_all_projects(user_argument.projects)
+    else:
+        all_projects = PROJECTS
     top_verbs = []
+    all_funcs_names = []
     existing_projects = get_existing_projects(all_projects)
     for project in existing_projects:
         print('dirpath: {dirpath}:'.format(dirpath=project))
@@ -96,24 +133,26 @@ if __name__ == '__main__':
         print('total ".py" files count: {count}'.format(count=len(filepaths)))
         files_content_list = get_file_content_list(filepaths)
         trees = get_trees(files_content_list)
-        print('trees generated')
         func_names = get_func_names(trees)
-        clear_func_names_list = filtering_funcs(sum(func_names, []))
-        print('functions extracted')
+        funcs_names_list = sum(func_names, [])
+        clear_func_names_list = filtering_funcs(funcs_names_list)
+        if user_argument.all_names:
+            print_all_project_funcs_names(funcs_names_list)
         verbs = []
         for function_name in clear_func_names_list:
             verbs.append([word for word in function_name.split('_')
                           if verb_check(word)])
-        verbs_counter = get_verbs(sum(verbs, []))
+        verbs_counter = collections.Counter(sum(verbs, []))
         for verb in verbs_counter:
             print('verb "{verb}" count: {count}'.format(
                 verb=verb, count=verbs_counter[verb]))
+        all_funcs_names += funcs_names_list
         top_verbs += verbs_counter
-        print('\n')
+        print('------------')
+    if user_argument.all_names:
+        print_total_funcs_names_info(all_funcs_names)
+    print_verbs_info(top_verbs)
 
-    print('total {words} words, unique {unique}'.format(
-        words=len(top_verbs), unique=len(set(top_verbs))))
-    for word, occurence in collections.Counter(
-            top_verbs).most_common():
-        print('"{verb}" in {occurence} projects'.format(verb=word,
-                                                        occurence=occurence))
+
+if __name__ == '__main__':
+    output_to_cli()
