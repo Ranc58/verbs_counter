bb9970a034a06875f51fba566a7ea9aacb96aa99
Ranc58 <rancvova@gmail.com>
2017-10-07 19:44:50 +0300

    fix bugs with import in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
-import words_counter
+
 
 setup(
     name='words_counter',
-    version='1.3.4',
+    version='1.3.7',
     packages=find_packages(),
     author='Vladimir Aleshin',
     author_email='rancvova@gmail.com',
@@ -16,8 +16,11 @@ setup(
             ['words_counter = words_counter.words_counter:main']
     },
     install_requires=[
+        'six==1.11.0',
         'nltk==3.2.4',
-        'GitPython==2.1.7'
+        'GitPython==2.1.7',
     ],
     test_suite='words_counter.tests',
 )
+
+
